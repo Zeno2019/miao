4e4d19d9b86ee6988a9d3093ec4d7f3fd8d52f54
Zeno <lin1071833138@gmail.com>
2020-04-28 16:04:41 +0800

    add Zeno2019-lodash.js

---
+++ lodash/Zeno2019-lodash.js
@@ -0,0 +1,16 @@
+/**
+ * 只要可以通过 username.xx 的方式调用即可
+ *
+ * 注释方式
+ *
+ */
+
+var Zeno2019 = {};
+
+Zeno2019.isNull = function (val) {
+  if (val === null) {
+    return true;
+  } else {
+    return false;
+  }
+}
