d10de014efa8334a25167fbe14c73bc0fb712a47
Zeno <lin1071833138@gmail.com>
2020-04-28 18:45:24 +0800

    add .chunk

--- lodash/zeno2019-lodash.js
+++ lodash/zeno2019-lodash.js
@@ -1,6 +1,36 @@
-//
+/**
+ *
+ *
+ */
+
 var zeno2019 = {};
 
 zeno2019.isNull = function (val) {
   return val === null ? true : false;
+}
+
+zeno2019.chunk = function (array, size = 1) {
+  var result = [];
+
+  if (array == null || size < 1) {
+    return result;
+  }
+
+  var length = array.length;
+  var tempArray = [];
+  var arrIndex = 0;
+  var rIndex = 0;
+
+  while (arrIndex < length) {
+    tempArray.push(array[arrIndex++]);
+    if (arrIndex % size == 0) {
+      result[rIndex++] = tempArray;
+      tempArray = [];
+    }
+  }
+  if (length % size > 0) {
+    result[rIndex] = tempArray;
+  }
+
+  return result;
 }
